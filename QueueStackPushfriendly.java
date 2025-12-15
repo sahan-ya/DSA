7c10f300c9c301306917841fc622cb80c2ebd705
Sahanya <dulanisahanya227@gmail.com>
2025-12-16 01:57:14 +0530

    Update QueueStackPushfriendly.java

--- QueueStackPushfriendly.java
+++ QueueStackPushfriendly.java
@@ -1,1 +1,46 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueStackPushFriendly {
+
+    Queue<Integer> q1 = new LinkedList<>();
+    Queue<Integer> q2 = new LinkedList<>();
+
+    // Push operation (O(1))
+    public void push(int x) {
+        q1.add(x);
+    }
+
+    // Pop operation (O(n))
+    public int pop() {
+        if (q1.isEmpty()) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+
+        // Move elements except last to q2
+        while (q1.size() > 1) {
+            q2.add(q1.remove());
+        }
+
+        int popped = q1.remove();
+
+        // Swap q1 and q2
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+
+        return popped;
+    }
+
+    public static void main(String[] args) {
+        QueueStackPushFriendly stack = new QueueStackPushFriendly();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println(stack.pop()); // 30
+        System.out.println(stack.pop()); // 20
+    }
+}
 
