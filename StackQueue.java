5fb4bf81bbd09d69fe10e3646420dbefc58a636a
Sahanya <dulanisahanya227@gmail.com>
2025-12-16 20:45:56 +0530

    Update StackQueue.java

--- StackQueue.java
+++ StackQueue.java
@@ -1,1 +1,47 @@
+import java.util.Stack;
 
+//Enqueue Friendly
+public class StackQueue {
+
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    // Enqueue operation (O(1))
+    public void enqueue(int x) {
+        s1.push(x);
+    }
+
+    // Dequeue operation (O(n))
+    public int dequeue() {
+        if (s1.isEmpty() && s2.isEmpty()) {
+            System.out.println("Queue is empty");
+            return -1;
+        }
+
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+
+        return s2.pop();
+    }
+    
+    // Print queue elements
+    public void printQueue() {
+    System.out.println("Queue elements: " + s1 + s2);
+}
+
+
+    public static void main(String[] args) {
+        StackQueue q = new StackQueue();
+        q.enqueue(100);
+        q.enqueue(200);
+        q.enqueue(300);
+
+        q.printQueue();
+        System.out.println("Popped: " + q.dequeue()); // 100
+        System.out.println("Popped: " + q.dequeue()); // 200
+        q.printQueue();
+    }
+}
